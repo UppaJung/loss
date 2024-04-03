048a450fbc01cf400464416e20c49f29f744f93e
Stuart Schechter <stuart@dicekeys.com>
2024-04-02 23:12:40 -0400

    polish survey output code

--- src/survey-converter/qsf.ts
+++ src/survey-converter/qsf.ts
@@ -5,6 +5,7 @@ export type FlowType = Exclude<Flow["Type"], "Standard" | "Root"> | "Block";
 
 export interface AugmentedSurveyQuestion extends SurveyQuestion {
   skipToEndOfBlockIfChoices?: Choice[];
+  isLikert7: boolean;
 }
 
 export interface LeafFlowNode {
@@ -51,14 +52,18 @@ const getFlowTree = (
 						console.warn(`Question with ID ${element.QuestionID} not found`);
 						return;
 					}
+          const {Payload} = question;
+          const {Choices} = Payload;
+          const isLikert7 = (Choices != null && Object.values(Choices).length == 7 &&
+            Object.values(Choices).map( c => c.Display ).sort().filter( (s, index) => s == (index+1).toString()).length === 7);
           const skipToEndOfBlockIfChoices = (SkipLogic == null) ? [] :
             SkipLogic.map( skipLogic => {
-              if (skipLogic.SkipToDestination === "ENDOFBLOCK" && skipLogic.QuestionID == question.Payload.QuestionID && skipLogic.Condition === "Selected" ) {
+              if (skipLogic.SkipToDestination === "ENDOFBLOCK" && skipLogic.QuestionID == Payload.QuestionID && skipLogic.Condition === "Selected" ) {
                 const option = (skipLogic.Locator.split("/").pop() ?? "") as `${number}`;
-                return question.Payload.Choices?.[option];
+                return Choices?.[option];
               }
             }).filter( x => x != null) as Choice[]
-					return {...question, skipToEndOfBlockIfChoices};
+					return {...question, isLikert7, skipToEndOfBlockIfChoices};
 				})
 				.filter( (question) => question != null) as AugmentedSurveyQuestion[];
 			return {
@@ -92,90 +97,131 @@ const getSortedQuestions = (
   }
 }
 
-const choicesToMarkdown = ({Choices, ChoiceOrder}: SurveyQuestionPayload) => {
-	if (Choices == null) {
-		return "";
-	}
-	const choiceObjects = ChoiceOrder == null ? Object.values(Choices) :
-		ChoiceOrder.map( index => Choices[`${index}`]);
-	const choiceStrings = choiceObjects.map( (choice) => choice.Display );
-	if (choiceStrings.length === 7 &&
-		choiceStrings.filter( (s, index) => s == (index+1).toString()).length === 7) {
-			const spaces5 = "&nbsp;&nbsp;&nbsp;&nbsp;";
-		return `\n${spaces5}${spaces5} [ 1 ]${spaces5}[ 2 ]${spaces5}[ 3 ]${spaces5}[ 4 ]${spaces5}[ 5 ]${spaces5}[ 6 ]${spaces5}[ 7 ]&nbsp;&nbsp;&nbsp;`;
-	}
-	const choiceLines = '\n\n  - ' + choiceStrings.join('\n  - ');
-	return choiceLines;
-}
-
-
-const html = String.raw;
 const htmlEncode = (s: string) => s.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
 
-const questionsToMd = (questions: AugmentedSurveyQuestion[]) => questions.map( q => {
-	const p = q.Payload;
-  const skip = (q.skipToEndOfBlockIfChoices == null || q.skipToEndOfBlockIfChoices.length === 0) ? "" :
-    `\n\nSkip to end of block if participant selects ${
-      q.skipToEndOfBlockIfChoices.map( c => `\`${htmlEncode(c.Display)}\``).join(", ")
-    }`;
-  return `${p.QuestionText}${choicesToMarkdown(p)}${skip}`;
-}).join("\n\n");
+
+// const choicesToMarkdown = ({Choices, ChoiceOrder}: SurveyQuestionPayload) => {
+// 	if (Choices == null) {
+// 		return "";
+// 	}
+// 	const choiceObjects = ChoiceOrder == null ? Object.values(Choices) :
+// 		ChoiceOrder.map( index => Choices[`${index}`]);
+// 	const choiceStrings = choiceObjects.map( (choice) => choice.Display );
+// 	if (choiceStrings.length === 7 &&
+// 		choiceStrings.filter( (s, index) => s == (index+1).toString()).length === 7) {
+// 			const spaces5 = "&nbsp;&nbsp;&nbsp;&nbsp;";
+// 		return `\n${spaces5}${spaces5} [ 1 ]${spaces5}[ 2 ]${spaces5}[ 3 ]${spaces5}[ 4 ]${spaces5}[ 5 ]${spaces5}[ 6 ]${spaces5}[ 7 ]&nbsp;&nbsp;&nbsp;`;
+// 	}
+// 	const choiceLines = '\n\n  - ' + choiceStrings.join('\n  - ');
+// 	return choiceLines;
+// }
 
 
-const choicesToHtml = ({Choices, ChoiceOrder}: SurveyQuestionPayload, lineSep: string) => {
-	if (Choices == null) {
-		return "";
-	}
-	const choiceObjects = ChoiceOrder == null ? Object.values(Choices) :
-		ChoiceOrder.map( index => Choices[`${index}`]);
-	const choiceStrings = choiceObjects.map( (choice) => choice.Display );
-  const likert = (choiceStrings.length === 7 &&
-		choiceStrings.filter( (s, index) => s == (index+1).toString()).length === 7);
-	const choiceLines = html`${lineSep
-    }<ul class="${likert ? 'choices-likert' : 'choices'}">${
-    choiceStrings.map( choice =>
-      html`${lineSep + "\t"
-        }<li class="${likert ? 'choice-likert' : 'choice'}">${htmlEncode(choice)}</li>`
-    ).join("")
-  }${lineSep}</ul>`;
-	return choiceLines;
+// const questionsToMd = (questions: AugmentedSurveyQuestion[]) => questions.map( q => {
+// 	const p = q.Payload;
+//   const skip = (q.skipToEndOfBlockIfChoices == null || q.skipToEndOfBlockIfChoices.length === 0) ? "" :
+//     `\n\nSkip to end of block if participant selects ${
+//       q.skipToEndOfBlockIfChoices.map( c => `\`${htmlEncode(c.Display)}\``).join(", ")
+//     }`;
+//   return `${p.QuestionText}${choicesToMarkdown(p)}${skip}`;
+// }).join("\n\n");
+
+
+export const surveyFormatterFactory = (callbacks: {
+  onBlockRandomizer: (args: {depth: number, children: string[]}) => string,
+  onGroup: (args: {depth: number, children: string[]}) => string,
+  onBlock: (args: {depth: number, description: string, children: string[]}) => string,
+  onQuestion: (args: {depth: number, children: string[], questionText: string, raw: AugmentedSurveyQuestion}) => string,
+  onChoice: (args: {depthOfQuestion: number, choiceString: string, choiceIndex: number, choice: Choice, allChoices: Choice[]}) => string,
+}) => {
+  const recursive = (flowNode: FlowNode, depth: number): string => {
+    if (flowNode.type === "BlockRandomizer") {
+      const children = flowNode.children.map( child => recursive(child, depth + 1));
+      return callbacks.onBlockRandomizer({depth, children});
+    } else if (flowNode.type === "Group") {
+      const children = flowNode.children.map( child => recursive(child, depth + 1));
+      return callbacks.onGroup({depth, children});
+    } else if (flowNode.type === "Block") {
+      const children = flowNode.questions.map( q => {
+        const {QuestionText: questionText, Choices, ChoiceOrder } = q.Payload;
+        const choiceObjects = Choices == null ? [] :
+          ChoiceOrder == null ? Object.values(Choices ?? []) :
+          ChoiceOrder.map( index => Choices[`${index}`]);
+        const children = choiceObjects.map( (choice, choiceIndex) => 
+          callbacks.onChoice({depthOfQuestion: depth + 1, choiceString: choice.Display, choiceIndex, choice, allChoices: choiceObjects}));
+        return callbacks.onQuestion({depth: depth + 1, questionText, children, raw: q});
+      });
+      return callbacks.onBlock({depth, description: flowNode.description, children})
+    } else {
+      return "";
+    }
+  }
+  return (flowNodeOrNodes: FlowNode | FlowNode[]) => {
+    if (Array.isArray(flowNodeOrNodes)) {
+      return recursive({type: "Group", children: flowNodeOrNodes}, 0);
+    } else {
+      return recursive(flowNodeOrNodes, 0);
+    }
+  }
 }
 
-const questionsToHtml = (flowOrFlows: FlowNode | FlowNode[], lineSep: string = "\n"): string => {
-  if (Array.isArray(flowOrFlows)) {
-    return flowOrFlows.map ( flow => questionsToHtml(flow, lineSep)).join("");
-  }
-  const flow = flowOrFlows;
-  if (flow.type === "BlockRandomizer" || flow.type === "Group") {
-    return `${lineSep}<div class="${flow.type}">${lineSep + "\t"
-     }<div class="block-header">${
-      flow.type === "BlockRandomizer" ?
-        "Randomize ordering" :
-        "Group"
-     }</div>${
-      questionsToHtml(flow.children, lineSep + "\t")
-    }${lineSep}</div>`;
-  } else if (flow.type === "Block") {
-    return html`${lineSep}<div class="${flow.type}">${lineSep + "\t"
-  }<div class="block-header">Block</div>${
-      flow.questions.map( q => {
-        const p = q.Payload;
-        const skip = (q.skipToEndOfBlockIfChoices == null || q.skipToEndOfBlockIfChoices.length === 0) ? "" :
-          html`${lineSep + "\t"}<div class="note">Skip to end of block if participant selects ${
-            q.skipToEndOfBlockIfChoices.map( c => `<span class="skip-option">${htmlEncode(c.Display)}</span>`).join(", ")
-          }</div>`;
-        return html`${lineSep + "\t"}<div class="question">${
-          lineSep + "\t\t"}<div class="question-body">${
-          p.QuestionText}${choicesToHtml(p, lineSep + "\t\t\t")
-        }${lineSep + "\t\t"}</div>${
-        lineSep + "\t"}</div>${skip}`;
-      }).join("")
-    }${lineSep}</div>`;
+const depthToTabs = (depth: number) => "\t".repeat(depth);
+
+const tag = (
+  tagName: string,
+  {depth, class: classes, singleLine=false}: {depth: number, class?: string | string[], singleLine?: boolean},
+  ...content: string[]
+) => {
+  const openTag = `<${tagName}${classes == null ? "" : ` class="${Array.isArray(classes) ? classes.join(" ") : classes}"`}>`;
+  const closeTag = `</${tagName}>`;
+  if (singleLine) {
+    return `${depthToTabs(depth)}${openTag}${content.join("")}${closeTag}${"\n"}`;
   } else {
-    return "";
+    return `${depthToTabs(depth)}${openTag}${"\n"}${content.join("")}${"\n"}${depthToTabs(depth)}${closeTag}${"\n"}`;
   }
 }
 
+const htmlFormatter = surveyFormatterFactory({
+  onBlockRandomizer: ({depth, children}) =>
+    tag("div", {class: "BlockRandomizer", depth}, 
+      tag("div", {class: "block-header", depth: depth + 1, singleLine: true}, "Randomize"),
+      children.join("")
+    ),
+  onGroup: ({depth, children}) => 
+    tag("div", {class: "Group", depth}, 
+      tag("div", {class: "block-header", depth: depth + 1, singleLine: true}, "Group"),
+      children.join("")
+    ),
+  onBlock: ({depth, description, children}) =>
+      tag("div", {class: "Block", depth}, 
+        tag("div", {class: "block-header", depth: depth + 1, singleLine: true}, `Block: ${htmlEncode(description)} (block names not exposed to participants)`),
+        children.join("")
+    ),
+  onChoice: ({depthOfQuestion, choiceString}) =>
+    tag('li', {class: "choice", depth: depthOfQuestion + 2, singleLine: true}, htmlEncode(choiceString)),
+  onQuestion: ({depth, questionText, children, raw}) => {
+    const {skipToEndOfBlockIfChoices, isLikert7} = raw;
+    const classes = ["question", ...(isLikert7 ? ["likert"] : [])];
+    return tag("div", {depth, class: classes}, 
+      tag("div", {class: "question-body", depth: depth + 1, singleLine: true}, questionText),
+      children.length == 0 ? "" : tag("ul", {class: "choices", depth: depth + 1}, ...children),
+      (skipToEndOfBlockIfChoices == null || skipToEndOfBlockIfChoices.length === 0) ? "" :
+        tag("div", {depth, class: "note", singleLine: true},
+          `Skip to end of block if participant selects `,
+          skipToEndOfBlockIfChoices.map( c =>
+              tag("span", {class: "skip-option", depth: depth + 1, singleLine: true}, htmlEncode(c.Display))
+          ).join(", ")
+       )
+    );
+    // ) html`${depthToTabs(depth)}<div class="question${isLikert7 ? ' likert' : ''}">${"\n"}${depthToTabs(depth + 1)
+    //   }<div class="question-body">${"\n"}${depthToTabs(depth + 2)
+    //     }${questionText}${"\n"}${depthToTabs(depth + 1)
+    //     }</div>${"\n"}${children.length == 0 ? "" :  `${depthToTabs(depth + 1)
+    //       }<ul class="choices">${"\n" + children.join("") + depthToTabs(depth + 1)}</ul>${"\n"}`
+    //     }${skip}${depthToTabs(depth)
+    //  }</div>${"\n"}`;
+  },
+})
 const parseQsf = (path: string) => {
   const json = Deno.readTextFileSync(path);
 	const qsfObject = JSON.parse(json) as SurveyRoot;
@@ -209,11 +255,8 @@ const parse = () => {
 	Deno.writeTextFileSync("./src/survey-converter/out/questions-ordered.json",
 		JSON.stringify(questionsInFlowOrder, null, "\t")
 	);
-	Deno.writeTextFileSync("./questions-ordered.md",
-		questionsToMd(questionsInFlowOrder)
-	);
   Deno.writeTextFileSync("./survey.vto",
-    defaultHeader + questionsToHtml(flowTree)
+    defaultHeader + htmlFormatter(flowTree)
       .replaceAll("$\{q:\/\/QID2\/ChoiceTextEntryValue\}", '<i>participant\'s top harm</i>')
       .replaceAll("$\{q:\/\/QID5\/ChoiceTextEntryValue\}", '<i>participant\'s second harm</i>')
       .replaceAll("$\{q:\/\/QID6\/ChoiceTextEntryValue\}", '<i>participant\'s third harm</i>')
